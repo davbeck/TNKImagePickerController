aba232cd6a75a113d685b8e38d91def45b8c25f8
David Beck <code@thinkultimate.com>
2016-05-11 11:38:48 -0700

    Fixed asset order in inverse layout
    Because we invert the collection view in moments, we also need to invert the asset rows.

--- TNKImagePickerController/TNKCollectionViewInvertedFlowLayout.m
+++ TNKImagePickerController/TNKCollectionViewInvertedFlowLayout.m
@@ -11,16 +11,41 @@
 
 @implementation TNKCollectionViewInvertedFlowLayout
 
+- (CGRect)_rectForSection:(NSInteger)section {
+	NSInteger items = [self.collectionView numberOfItemsInSection:section];
+	if (items == 0) {
+		return CGRectNull;
+	}
+	
+	NSIndexPath *firstIndexPath = [NSIndexPath indexPathForItem:0 inSection:section];
+	NSIndexPath *lastIndexPath = [NSIndexPath indexPathForItem:items - 1 inSection:section];
+	
+	UICollectionViewLayoutAttributes *firstAttributes = [super layoutAttributesForItemAtIndexPath:firstIndexPath];
+	UICollectionViewLayoutAttributes *lastAttributes = [super layoutAttributesForItemAtIndexPath:lastIndexPath];
+	
+	return CGRectMake(firstAttributes.frame.origin.x,
+					  firstAttributes.frame.origin.y,
+					  CGRectGetMaxX(lastAttributes.frame) - CGRectGetMinX(firstAttributes.frame),
+					  CGRectGetMaxY(lastAttributes.frame) - CGRectGetMinY(firstAttributes.frame));
+}
+
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
-	UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForItemAtIndexPath:indexPath];
+	UICollectionViewLayoutAttributes *attributes = [[super layoutAttributesForItemAtIndexPath:indexPath] copy];
 	
 	attributes.transform = TNKInvertedTransform;
+	
+	CGRect sectionRect = [self _rectForSection:indexPath.section];
+	if (!CGRectIsNull(sectionRect)) {
+		CGRect frame = attributes.frame;
+		frame.origin.y = sectionRect.origin.y + CGRectGetMaxY(sectionRect) - CGRectGetMaxY(frame);
+		attributes.frame = frame;
+	}
 	
 	return attributes;
 }
 
 - (UICollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
-	UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath];
+	UICollectionViewLayoutAttributes *attributes = [[super layoutAttributesForSupplementaryViewOfKind:kind atIndexPath:indexPath] copy];
 	
 	attributes.transform = TNKInvertedTransform;
 	
@@ -28,7 +53,7 @@
 }
 
 - (nullable UICollectionViewLayoutAttributes *)layoutAttributesForDecorationViewOfKind:(NSString*)elementKind atIndexPath:(NSIndexPath *)indexPath {
-	UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForDecorationViewOfKind:elementKind atIndexPath:indexPath];
+	UICollectionViewLayoutAttributes *attributes = [[super layoutAttributesForDecorationViewOfKind:elementKind atIndexPath:indexPath] copy];
 	
 	attributes.transform = TNKInvertedTransform;
 	
@@ -36,13 +61,32 @@
 }
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
-	NSArray *answer = [super layoutAttributesForElementsInRect:rect];
+	NSArray *originalAnswer = [super layoutAttributesForElementsInRect:rect];
+	NSMutableOrderedSet *answer = [NSMutableOrderedSet orderedSetWithArray:originalAnswer];
 	
-	for (UICollectionViewLayoutAttributes *attributes in answer) {
+	NSMutableIndexSet *sections = [NSMutableIndexSet new];
+	
+	for (UICollectionViewLayoutAttributes *attributes in originalAnswer) {
 		attributes.transform = TNKInvertedTransform;
+		
+		if (attributes.representedElementCategory == UICollectionElementCategoryCell) {
+			[answer removeObject:attributes];
+			
+			[sections addIndex:attributes.indexPath.section];
+		}
 	}
 	
-	return answer;
+	[sections enumerateIndexesUsingBlock:^(NSUInteger section, BOOL * _Nonnull stop) {
+		for (NSInteger item = 0; item < [self.collectionView numberOfItemsInSection:section]; item++) {
+			UICollectionViewLayoutAttributes *attributes = [self layoutAttributesForItemAtIndexPath:[NSIndexPath indexPathForItem:item inSection:section]];
+			
+			if (CGRectIntersectsRect(rect, attributes.frame)) {
+				[answer addObject:attributes];
+			}
+		}
+	}];
+	
+	return answer.array;
 }
 
 @end
